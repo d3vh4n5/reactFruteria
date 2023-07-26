f6fddf21afe46f6187e2a11cc3dab1145492b630
Juan <juanangelbasgall@hotmail.com>
2023-07-26 00:42:30 -0300

    Agregadas tarjetas, fetch al json local, estilos al nav, y algunos detalitos en general

--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import frutas from "../img/icon-fruits-64.png";
 
 const Carrito = () => {
   return (
-    <div>Carrito</div>
+    <div>
+      <h1>Aca va la tabla del carrito</h1>
+      <img src={frutas} className="logo" alt="logo" />
+    </div>
   )
 }
 
